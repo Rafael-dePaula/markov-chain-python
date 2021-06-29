8444b195ff04a66fb531f825c6f1b560476893a6
Rafael-dePaula <78245128+Rafael-dePaula@users.noreply.github.com>
2021-06-28 21:48:12 -0300

    Add files via upload

---
+++ markov.py
@@ -0,0 +1,11 @@
+from pomegranate import *
+d1 = DiscreteDistribution({'sol': 0.5, 'chuva': 0.5})
+d2 = ConditionalProbabilityTable([['sol', 'sol', 0.9],
+                                      ['sol', 'chuva', 0.1],
+                                      ['chuva', 'sol', 0.6],
+                                      ['chuva', 'chuva', 0.4]], [d1])
+
+model = MarkovChain([d1, d2])
+
+for valor in model.sample(10):
+    print (valor)
